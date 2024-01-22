cacf2b8610eecf54f5653c5714f89d71ca6d328a
Sinayra Moreira <sinayra.moreira@sonova.com>
2024-01-22 17:37:02 +0100

    Removed comment

--- src/status/model.ts
+++ src/status/model.ts
@@ -1,6 +1,6 @@
 const allStatus = ['Happy', 'Sad', 'Hungry'] as const;
 export type Status = (typeof allStatus)[number];
 
-export type BadStatus = Extract<Status, 'Sad' | 'Hungry'>; // Generate routes error. GenerateMetadataError: Maximum call stack size exceeded
+export type BadStatus = Extract<Status, 'Sad' | 'Hungry'>;
 
 export type BadStatusRequest = BadStatus | Uppercase<BadStatus>;
